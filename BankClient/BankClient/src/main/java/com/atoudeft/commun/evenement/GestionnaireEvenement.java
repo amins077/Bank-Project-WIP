0a11d26b112e0e7507a21a01ededb4f50b141587
DarkMagicHD <aliatik457@gmail.com>
2024-11-14 17:46:41 -0500

    1.2 (v0.2.1)

--- BankClient/BankClient/src/main/java/com/atoudeft/commun/evenement/GestionnaireEvenement.java
+++ BankClient/BankClient/src/main/java/com/atoudeft/commun/evenement/GestionnaireEvenement.java
@@ -17,19 +17,4 @@ public interface GestionnaireEvenement {
 	* @param evenement L'événement à gérer.
 	*/
 	void traiter(Evenement evenement);
-	Client client;
-	switch (Evenement) {
-		case "END":
-			client.deconnecter();
-			break;
-		case "LIST":
-			String arg = evenement.getArgument();
-			membres = arg.split(":");
-			System.out.println("\t\t"+membres.length+" personnes dans le salon :");
-			for (String s:membres)
-				System.out.println("\t\t\t- "+s);
-			break;
-		default:
-			System.out.println("\t\t"+evenement.getType()+" "+evenement.getArgument());
 	}
-}
