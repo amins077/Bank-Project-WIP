fecfe4deb2fec4158e556cc8c9b9f4b9aef37052
DarkMagicHD <aliatik457@gmail.com>
2024-11-16 21:01:46 -0500

    7.2

--- BankServer/BankServer/src/main/java/com/atoudeft/banque/CompteBancaire.java
+++ BankServer/BankServer/src/main/java/com/atoudeft/banque/CompteBancaire.java
@@ -1,5 +1,7 @@
 package com.atoudeft.banque;
 
+import com.sun.org.apache.xpath.internal.operations.Operation;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -11,24 +13,26 @@ public abstract class CompteBancaire implements Serializable {
     /**
      * Génère un numéro de compte bancaire aléatoirement avec le format CCC00C, où C est un caractère alphabétique
      * majuscule et 0 est un chiffre entre 0 et 9.
+     *
      * @return
      */
     public static String genereNouveauNumero() {
         char[] t = new char[6];
-        for (int i=0;i<3;i++) {
-            t[i] = (char)((int)(Math.random()*26)+'A');
+        for (int i = 0; i < 3; i++) {
+            t[i] = (char) ((int) (Math.random() * 26) + 'A');
         }
-        for (int i=3;i<5;i++) {
-            t[i] = (char)((int)(Math.random()*10)+'0');
+        for (int i = 3; i < 5; i++) {
+            t[i] = (char) ((int) (Math.random() * 10) + '0');
         }
-        t[5] = (char)((int)(Math.random()*26)+'A');
+        t[5] = (char) ((int) (Math.random() * 26) + 'A');
         return new String(t);
     }
 
     /**
      * Crée un compte bancaire.
+     *
      * @param numero numéro du compte
-     * @param type type du compte
+     * @param type   type du compte
      */
     public CompteBancaire(String numero, TypeCompte type) {
         this.numero = numero;
@@ -39,15 +43,21 @@ public abstract class CompteBancaire implements Serializable {
     public String getNumero() {
         return numero;
     }
+
     public TypeCompte getType() {
         return type;
     }
+
     public double getSolde() {
         return solde;
     }
+
     public abstract boolean crediter(double montant);
+
     public abstract boolean debiter(double montant);
+
     public abstract boolean payerFacture(String numeroFacture, double montant, String description);
+
     public abstract boolean transferer(double montant, String numeroCompteDestinataire);
 
     protected void setSolde(double nouveauSolde) {
@@ -56,29 +66,128 @@ public abstract class CompteBancaire implements Serializable {
 
     //7.1
 
-    enum TypeOperation {
+    public enum TypeOperation {
         DEPOT,
         RETRAIT,
         TRANSFER,
         FACTURE
     }
-    abstract class Operation {
-        private TypeOperation type;
+
+    public abstract class Operation {
+
+        private CompteBancaire.TypeOperation type;
         private Date dateOperation;
 
         //Constructeur pour initialiser le type et la date de l'opération
-        public Operation(TypeOperation type) {
+        public Operation(CompteBancaire.TypeOperation type) {
             this.type = type;
             this.dateOperation = new Date(System.currentTimeMillis());
         }
+
         //Getter pour le type de l'opération
-        public TypeOperation getType() {
+        public CompteBancaire.TypeOperation getType() {
             return type;
         }
+
         //Getter pour la date de l'opération
         public Date getDateOperation() {
             return dateOperation;
         }
-        public abstract  void executer();
+
+        public abstract void executer();
+    }
+
+    //7.2
+
+    public class OperationDepot extends Operation {
+
+        private double montant;
+
+        public OperationDepot(double montant) {
+            super(TypeOperation.DEPOT);
+            this.montant = montant;
+        }
+
+        public double getMontant() {
+            return  montant;
+        }
+
+        @Override
+        public void executer() {
+            //Logique pour éxecuter l'opération de dépot
+            System.out.println("Dépôt de " + montant + " unités.");
+        }
+    }
+
+    public class OperationRetrait extends Operation {
+
+        private double montant;
+
+        public OperationRetrait(double montant) {
+            super(TypeOperation.RETRAIT);
+            this.montant = montant;
+        }
+
+        public double getMontant() {
+            return montant;
+        }
+
+        @Override
+        public void executer() {
+            //Logique pour éxecuter l'opération de retrait
+            System.out.println("Retrait de " + montant + " unités.");
+        }
+    }
+
+    public class OperationTransfer extends Operation {
+
+        private double montant;
+
+        public OperationTransfer(double montant) {
+            super(TypeOperation.TRANSFER);
+            this.montant = montant;
+        }
+
+        public double getMontant() {
+            return montant;
+        }
+
+        @Override
+        public void executer() {
+            //Logique pour éxecuter l'opération de transfer.
+            System.out.println("Transfer de " + montant + " unités");
+        }
+    }
+
+    public class OperationFacture extends Operation {
+
+        private double montant;
+        private String numeroFacture;
+        private String description;
+
+        public OperationFacture(double montant, String numeroFacture, String description) {
+            super(TypeOperation.FACTURE);
+            this.montant = montant;
+            this.numeroFacture = numeroFacture;
+            this.description = description;
+        }
+
+        public double getMontant() {
+            return montant;
+        }
+
+        public String getNumeroFacture() {
+            return numeroFacture;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public void executer() {
+            //Logique pour éxecuter l'opération de facture.
+            System.out.println("Paiement de la facture " + numeroFacture + " de " + montant + " unités pour " + description + ".");
+        }
     }
 }
